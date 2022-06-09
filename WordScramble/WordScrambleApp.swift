21b423f4fba227b25135e993c7da482fffe2780c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-09 16:46:10 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gabriel Marquez on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
